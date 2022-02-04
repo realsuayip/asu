0a56390e0854f642a29ca0df4144c6c6759070ac
suayip <suayip.541@gmail.com>
2022-02-05 00:32:34 +0300

    add an integration test for the registration process

---
+++ zeynep/tests/integration/test_registration.py
@@ -0,0 +1,53 @@
+import re
+
+from django.core import mail
+from django.urls import reverse
+from rest_framework.test import APITestCase
+
+from zeynep.auth.models import User
+from zeynep.verification.models import RegistrationVerification
+
+
+class RegistrationTest(APITestCase):
+    """
+    Test the entire registration process.
+    """
+
+    def test_registration(self):
+        test_backend = "django.core.mail.backends.locmem.EmailBackend"
+        url_check = reverse("registration-verification-check")
+        url_send = reverse("registration-verification-list")
+        url_register = reverse("user-list")
+        email = "test@example.com"
+
+        # Send code to e-mail
+        with self.settings(EMAIL_BACKEND=test_backend):
+            send_response = self.client.post(url_send, data={"email": email})
+            self.assertEqual(201, send_response.status_code)
+            self.assertEqual(1, len(mail.outbox))
+            (code,) = re.findall(r"[\d]{6}", mail.outbox[0].body)
+
+        # Check and verify the combination, get consent
+        check_response = self.client.post(
+            url_check, data={"email": email, "code": code}
+        )
+        self.assertEqual(200, check_response.status_code)
+        consent = check_response.data["consent"]
+
+        # Create the actual user
+        register_response = self.client.post(
+            url_register,
+            data={
+                "email": email,
+                "consent": consent,
+                "display_name": "Janet",
+                "username": "janet_52",
+                "gender": "female",
+                "birth_date": "2000-01-01",
+            },
+        )
+        self.assertEqual(201, register_response.status_code)
+
+        # Let's make sure everything is in the database.
+        user = User.objects.get(email=email)
+        RegistrationVerification.objects.get(user=user)

562614b3d61b5128e8d990e03b27b0ee3930ca63
suayip <suayip.541@gmail.com>
2022-03-19 00:10:38 +0300

    Break down serializers into modules in auth app

---
+++ zeynep/auth/serializers/user.py
@@ -0,0 +1,115 @@
+import django.core.validators
+from django.contrib.auth.password_validation import validate_password
+from django.db import transaction
+from django.utils import timezone
+from django.utils.translation import gettext
+
+from rest_framework import serializers
+
+from zeynep.auth.models import User
+from zeynep.verification.models import RegistrationVerification
+
+
+class UserPublicReadSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "display_name",
+            "username",
+            "date_joined",
+            "url",
+        )
+        extra_kwargs = {"url": {"lookup_field": "username"}}
+
+
+class UserPrivateReadSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "email",
+            "display_name",
+            "username",
+            "gender",
+            "birth_date",
+            "date_joined",
+            "url",
+        )
+        extra_kwargs = {"url": {"lookup_field": "username"}}
+
+
+class UserUpdateSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "display_name",
+            "username",
+            "gender",
+            "birth_date",
+            "url",
+        )
+        extra_kwargs = {"url": {"lookup_field": "username"}}
+
+
+class UserCreateSerializer(serializers.HyperlinkedModelSerializer):
+    consent = serializers.CharField(write_only=True)
+    password = serializers.CharField(
+        max_length=256,
+        write_only=True,
+        style={"input_type": "password"},
+    )
+
+    def validate_email(self, email):  # noqa
+        return User.objects.normalize_email(email)
+
+    @transaction.atomic
+    def create(self, validated_data):
+        consent = validated_data.pop("consent")
+        email = validated_data["email"]
+
+        verification = RegistrationVerification.objects.get_with_consent(
+            email, consent
+        )
+
+        if verification is None:
+            raise serializers.ValidationError(
+                {
+                    "email": gettext(
+                        "This e-mail could not be verified."
+                        " Please provide a validated e-mail address."
+                    )
+                }
+            )
+
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+
+        try:
+            validate_password(password, user=user)
+        except django.core.validators.ValidationError as err:
+            raise serializers.ValidationError({"password": err.messages})
+
+        user.set_password(password)
+        user.save()
+        verification.user = user
+        verification.date_completed = timezone.now()
+        verification.save(update_fields=["user", "date_completed"])
+        verification.null_others()
+        return user
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "email",
+            "display_name",
+            "username",
+            "password",
+            "gender",
+            "birth_date",
+            "consent",
+            "url",
+        )
+        extra_kwargs = {"url": {"lookup_field": "username"}}
--- zeynep/auth/views.py
+++ zeynep/auth/views.py
@@ -1,170 +1,19 @@
-import django.core.validators
-from django.contrib.auth.password_validation import validate_password
-from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.utils import timezone
-from django.utils.translation import gettext
 
-from rest_framework import permissions, serializers, status
+from rest_framework import permissions, status
 from rest_framework.decorators import action
-from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 
 from zeynep.auth.models import User
-from zeynep.utils.views import ExtendedViewSet
-from zeynep.verification.models import (
-    PasswordResetVerification,
-    RegistrationVerification,
+from zeynep.auth.serializers.actions import PasswordResetSerializer
+from zeynep.auth.serializers.user import (
+    UserCreateSerializer,
+    UserPrivateReadSerializer,
+    UserPublicReadSerializer,
+    UserUpdateSerializer,
 )
-
-
-class UserPublicReadSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = (
-            "id",
-            "display_name",
-            "username",
-            "date_joined",
-            "url",
-        )
-        extra_kwargs = {"url": {"lookup_field": "username"}}
-
-
-class UserPrivateReadSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = (
-            "id",
-            "email",
-            "display_name",
-            "username",
-            "gender",
-            "birth_date",
-            "date_joined",
-            "url",
-        )
-        extra_kwargs = {"url": {"lookup_field": "username"}}
-
-
-class UserUpdateSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = (
-            "id",
-            "display_name",
-            "username",
-            "gender",
-            "birth_date",
-            "url",
-        )
-        extra_kwargs = {"url": {"lookup_field": "username"}}
-
-
-class UserCreateSerializer(serializers.HyperlinkedModelSerializer):
-    consent = serializers.CharField(write_only=True)
-    password = serializers.CharField(
-        max_length=256,
-        write_only=True,
-        style={"input_type": "password"},
-    )
-
-    def validate_email(self, email):  # noqa
-        return User.objects.normalize_email(email)
-
-    @transaction.atomic
-    def create(self, validated_data):
-        consent = validated_data.pop("consent")
-        email = validated_data["email"]
-
-        verification = RegistrationVerification.objects.get_with_consent(
-            email, consent
-        )
-
-        if verification is None:
-            raise serializers.ValidationError(
-                {
-                    "email": gettext(
-                        "This e-mail could not be verified."
-                        " Please provide a validated e-mail address."
-                    )
-                }
-            )
-
-        password = validated_data.pop("password")
-        user = User(**validated_data)
-
-        try:
-            validate_password(password, user=user)
-        except django.core.validators.ValidationError as err:
-            raise serializers.ValidationError({"password": err.messages})
-
-        user.set_password(password)
-        user.save()
-        verification.user = user
-        verification.date_completed = timezone.now()
-        verification.save(update_fields=["user", "date_completed"])
-        verification.null_others()
-        return user
-
-    class Meta:
-        model = User
-        fields = (
-            "id",
-            "email",
-            "display_name",
-            "username",
-            "password",
-            "gender",
-            "birth_date",
-            "consent",
-            "url",
-        )
-        extra_kwargs = {"url": {"lookup_field": "username"}}
-
-
-class PasswordResetSerializer(serializers.Serializer):  # noqa
-    email = serializers.EmailField()
-    consent = serializers.CharField(write_only=True)
-    password = serializers.CharField(
-        write_only=True,
-        style={"input_type": "password"},
-    )
-
-    def validate_email(self, email):  # noqa
-        return User.objects.normalize_email(email)
-
-    def create(self, validated_data):
-        password = validated_data["password"]
-        email = validated_data["email"]
-
-        try:
-            user = User.objects.get(email=email)
-        except User.DoesNotExist:
-            raise NotFound
-
-        try:
-            validate_password(password, user=user)
-        except django.core.validators.ValidationError as err:
-            raise serializers.ValidationError({"password": err.messages})
-
-        verification = PasswordResetVerification.objects.get_with_consent(
-            email, validated_data["consent"], user=user
-        )
-
-        if verification is None:
-            raise serializers.ValidationError(
-                {"email": gettext("This e-mail could not be verified.")}
-            )
-
-        verification.date_completed = timezone.now()
-        verification.save(update_fields=["date_completed"])
-        verification.null_others()
-
-        user.set_password(password)
-        user.save(update_fields=["password"])
-        return validated_data
+from zeynep.utils.views import ExtendedViewSet
 
 
 class UserPermissions(permissions.IsAuthenticatedOrReadOnly):

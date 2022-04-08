59c78184e715c9de9383d97a267732529b95bf75
şuayip üzülmez <suayip.541@gmail.com>
2022-04-08 22:53:38 +0300

    Improve logic for user block/follow

--- zeynep/auth/serializers/actions.py
+++ zeynep/auth/serializers/actions.py
@@ -1,11 +1,12 @@
 import django.core.validators
 from django.contrib.auth.password_validation import validate_password
 from django.db import transaction
+from django.db.models import Q
 from django.utils import timezone
 from django.utils.translation import gettext
 
 from rest_framework import serializers
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, PermissionDenied
 
 from zeynep.auth.models import User, UserBlock, UserFollow
 from zeynep.verification.models import PasswordResetVerification
@@ -67,10 +68,30 @@ class BlockSerializer(serializers.ModelSerializer):
         fields = ("from_user", "to_user")
         extra_kwargs = {"from_user": {"write_only": True}}
 
+    def get_rels(self, model, *, from_user, to_user):  # noqa
+        # Given m2m through model, return queryset
+        # containing objects for both directions
+        return model.objects.filter(
+            (Q(from_user=from_user) & Q(to_user=to_user))
+            | (Q(to_user=from_user) & Q(from_user=to_user))
+        )
+
+    @transaction.atomic
     def create(self, validated_data):
-        return self.Meta.model.objects.get_or_create(**validated_data)
+        # If there is a follow relationship between
+        # users, delete it before blocking.
+        self.get_rels(UserFollow, **validated_data).delete()
+        return UserBlock.objects.get_or_create(**validated_data)
 
 
 class FollowSerializer(BlockSerializer):
     class Meta(BlockSerializer.Meta):
         model = UserFollow
+
+    def create(self, validated_data):
+        # If users block each other in any
+        # direction, following is not allowed.
+        blocks = self.get_rels(UserBlock, **validated_data)
+        if blocks.exists():
+            raise PermissionDenied
+        return UserFollow.objects.get_or_create(**validated_data)

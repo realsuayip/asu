61d2daba432a755be79b55803a30712412e2932f
şuayip üzülmez <suayip.541@gmail.com>
2022-07-25 23:46:33 +0300

    🤨

--- zeynep/utils/file.py
+++ zeynep/utils/file.py
@@ -12,7 +12,7 @@ import magic
 def get_mime_type(file):
     initial_pos = file.tell()
     file.seek(0)
-    mime_type = magic.from_buffer(file.read(1024), mime=True)
+    mime_type = magic.from_buffer(file.read(2048), mime=True)
     file.seek(initial_pos)
     return mime_type
 

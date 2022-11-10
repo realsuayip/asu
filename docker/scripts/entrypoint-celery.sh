264817c1a6f6dda043c41ad468f83f8708ccc978
şuayip üzülmez <suayip.541@gmail.com>
2022-11-10 23:09:48 +0300

    Update to Python 3.11 w/ optimized docker setup

--- docker/scripts/entrypoint-celery.sh
+++ docker/scripts/entrypoint-celery.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-if [ "$DATABASE" = "postgres" ]; then
-  while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
-    sleep 0.1
-  done
-fi
+while ! nc -z "$DATABASE_HOST" "$DATABASE_PORT"; do
+  sleep 0.1
+done
 
 exec "$@"
--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-if [ "$DATABASE" = "postgres" ]; then
-  echo "Checking for postgres..."
-
-  while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
-    sleep 0.1
-  done
-
-  echo "PostgreSQL is up."
-fi
+while ! nc -z "$DATABASE_HOST" "$DATABASE_PORT"; do
+  sleep 0.1
+done
 
 python manage.py migrate
-
 exec "$@"

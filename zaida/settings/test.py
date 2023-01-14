4e809ae55a91c5607d27885edd4ade9f39a1d258
şuayip üzülmez <suayip.541@gmail.com>
2023-01-14 14:58:21 +0300

    Circumvent test failures present in redis channel layer

--- zaida/settings/test.py
+++ zaida/settings/test.py
@@ -3,3 +3,12 @@ from zaida.settings import *  # noqa
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.MD5PasswordHasher",
 ]
+
+# Tests are failing on 'RedisChannelLayer' due to a bug present in
+# channels v4, until it is fixed, run tests on 'InMemoryChannelLayer'.
+CHANNEL_LAYERS = {
+    # Fixme later.
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    }
+}

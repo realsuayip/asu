26d9d135a46a05b2a8a8195a4306d2eee5683fb4
şuayip üzülmez <suayip.541@gmail.com>
2023-02-03 22:15:33 +0300

    Relocate Docker helper script

--- manage.py
+++ manage.py
@@ -4,16 +4,21 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    context = os.environ.get("CONTEXT")
+    # Default to docker helper module to work with Docker containers,
+    # then exit. For example:
+    #   $ CONTEXT=setup python manage.py up
+    # The command above will build and run all containers.
+    if context == "setup":
+        from docker.cli import entrypoint
+
+        entrypoint()
+
+    # The command is running inside the docker container, so run usual
+    # Django management stuff.
+    from django.core.management import execute_from_command_line
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zaida.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
     execute_from_command_line(sys.argv)
 
 

2bfc16cd7be28b6ac87cd9cb3a318942bff026fc
şuayip üzülmez <suayip.541@gmail.com>
2023-02-16 00:20:51 +0300

    Add dynamic project variable functionality

--- asu/settings/django.py
+++ asu/settings/django.py
@@ -84,6 +84,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            "libraries": {"asu": "asu.utils.templatetags"},
         },
     },
 ]

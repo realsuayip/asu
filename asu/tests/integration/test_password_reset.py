0a4bee0d53deaa509db6a84ed7e92e0e62dca0c6
şuayip üzülmez <suayip.541@gmail.com>
2023-02-16 23:52:29 +0300

    Adjust project variable functionality

--- asu/tests/integration/test_password_reset.py
+++ asu/tests/integration/test_password_reset.py
@@ -14,8 +14,6 @@ class PasswordResetTest(APITestCase):
     Test the entire password reset process.
     """
 
-    fixtures = ["project_variables"]
-
     @classmethod
     def setUpTestData(cls):
         cls.user = UserFactory(email="best@example.com")

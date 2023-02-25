b63ac35d5ed91856bdf060d6221f702ec529d19a
şuayip üzülmez <suayip.541@gmail.com>
2023-02-25 16:06:41 +0300

    Add view for api root & tidy up openapi docs

--- asu/settings/apps.py
+++ asu/settings/apps.py
@@ -1,3 +1,5 @@
+from django.urls import reverse_lazy
+
 from asu.utils.envparse import env
 
 REST_FRAMEWORK = {
@@ -12,9 +14,32 @@ REST_FRAMEWORK = {
     "EXCEPTION_HANDLER": "asu.utils.rest.exception_handler",
 }
 
+_system_font = """\
+-apple-system,BlinkMacSystemFont,"Segoe UI","Noto Sans",Helvetica,Arial,
+sans-serif,"Apple Color Emoji","Segoe UI Emoji"
+"""
+_system_font_mono = """\
+ui-monospace,SFMono-Regular,SF Mono,Menlo,Consolas,Liberation Mono,monospace
+"""
 SPECTACULAR_SETTINGS = {
     "VERSION": None,
     "TITLE": "asu API",
+    "SCHEMA_PATH_PREFIX": "/api",
+    "COMPONENT_SPLIT_REQUEST": True,
+    "REDOC_UI_SETTINGS": {
+        "expandResponses": "200,201",
+        "downloadFileName": "asu-openapi-spec",
+        "theme": {
+            "typography": {
+                "fontFamily": _system_font,
+                "code": {"fontFamily": _system_font_mono},
+                "headings": {"fontFamily": _system_font, "fontWeight": 600},
+            },
+        },
+    },
+    "OAUTH2_FLOWS": ["authorizationCode"],
+    "OAUTH2_AUTHORIZATION_URL": reverse_lazy("oauth2_provider:authorize"),
+    "OAUTH2_TOKEN_URL": reverse_lazy("oauth2_provider:token"),
 }
 
 REDIS_URL = env.str("REDIS_URL")
--- asu/urls.py
+++ asu/urls.py
@@ -1,32 +1,88 @@
 import importlib
+from collections import defaultdict
 
+from django import urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import URLResolver, include, path, reverse
 
-from rest_framework.routers import DefaultRouter
+from rest_framework.response import Response
+from rest_framework.routers import APIRootView, DefaultRouter
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 from oauth2_provider.urls import base_urlpatterns as oauth_urls
 from two_factor import views as tf
 
-router = DefaultRouter()
+
+class APIRoot(APIRootView):
+    namespaces = ("docs", "api", "oauth2_provider", "two_factor")
+
+    def _resolve_url(self, namespace, url):
+        try:
+            relpath = reverse(
+                namespace + ":" + url.name,
+                kwargs=url.pattern.regex.groupindex,
+            )
+        except urls.NoReverseMatch:
+            return None
+        return self.request.build_absolute_uri(relpath)
+
+    def _ordered(self, resolver):
+        try:
+            return self.namespaces.index(resolver.namespace)
+        except (ValueError, AttributeError):
+            return len(self.namespaces)
+
+    def get(self, request, *args, **kwargs):
+        url_resolver = urls.get_resolver(urls.get_urlconf())
+        resolvers = url_resolver.url_patterns
+
+        routes = defaultdict(list)
+        for resolver in sorted(resolvers, key=self._ordered):
+            if not isinstance(resolver, URLResolver):
+                continue
+
+            namespace = resolver.namespace
+            if namespace not in self.namespaces:
+                continue
+
+            values = []
+            for url in resolver.url_patterns:
+                name = url.name
+                pattern = str(url.pattern)
+
+                value = {"name": name, "pattern": pattern}
+                url = self._resolve_url(namespace, url)
+                if url is not None:
+                    value["url"] = url
+                values.append(value)
+
+            regex = str(resolver.pattern)
+            routes[regex] = values
+
+        ret = {
+            "status": "ok",
+            "version": request.version,
+            "user-agent": request.headers.get("user-agent"),
+            "routes": routes,
+        }
+        return Response(ret)
+
+
+class APIRouter(DefaultRouter):
+    include_format_suffixes = False
+    APIRootView = APIRoot
+
+
+router = APIRouter()
 router.root_view_name = "api-root"
 
 # Logic to scan through local apps and
 # register routers & custom urlpatterns
 
+api_urls = []
 local_apps = (app for app in settings.INSTALLED_APPS if app.startswith("asu."))
-api_urlpatterns = [
-    path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "schema/redoc/",
-        SpectacularRedocView.as_view(url_name="schema"),
-        name="redoc",
-    ),
-]
-
 for app in local_apps:
     try:
         module = importlib.import_module("%s.urls" % app)
@@ -40,7 +96,17 @@ for app in local_apps:
         router.registry.extend(app_router.registry)
 
     if app_urls is not None:
-        api_urlpatterns.extend(app_urls)
+        api_urls.extend(app_urls)
+
+
+docs_urls = [
+    path("schema/", SpectacularAPIView.as_view(), name="openapi-schema"),
+    path(
+        "",
+        SpectacularRedocView.as_view(url_name="docs:openapi-schema"),
+        name="browse",
+    ),
+]
 
 account_urls = [
     path("", tf.ProfileView.as_view(), name="profile"),
@@ -63,8 +129,9 @@ account_urls = [
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("account/", include((account_urls, "two_factor"))),
-    path("api/", include(router.urls + api_urlpatterns)),
+    path("api/", include((router.urls + api_urls, "api"))),
     path("o/", include((oauth_urls, "oauth2_provider"))),
+    path("docs/", include((docs_urls, "docs"))),
 ]
 
 if settings.DEBUG:

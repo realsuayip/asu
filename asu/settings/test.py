eba33dbb20d496f2ef1213a62e2e5d1cf3c14cc6
şuayip üzülmez <suayip.541@gmail.com>
2023-04-07 20:31:15 +0300

    Upgrade to Django 4.2

--- asu/settings/test.py
+++ asu/settings/test.py
@@ -3,14 +3,4 @@ from asu.settings import *  # noqa: F403
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.MD5PasswordHasher",
 ]
-
-# Tests are failing on 'RedisChannelLayer' due to a bug present in
-# channels v4, until it is fixed, run tests on 'InMemoryChannelLayer'.
-CHANNEL_LAYERS = {
-    # Fixme later.
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
-    }
-}
-
 INSTALLED_APPS.remove("debug_toolbar")  # noqa: F405

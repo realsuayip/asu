0a4a63b57dc6492507ddba1d8ecc0f1fee9624b1
şuayip üzülmez <suayip.541@gmail.com>
2023-04-20 00:33:48 +0300

    Improve i18n support

--- asu/utils/mailing.py
+++ asu/utils/mailing.py
@@ -2,8 +2,20 @@ from typing import Any
 
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
+from django.utils import translation
 
 from django_stubs_ext import StrOrPromise
+
+
+def determine_language(lang_code: str | None) -> str:
+    current = translation.get_language()
+    if lang_code is None:
+        return current
+
+    try:
+        return translation.get_supported_language_variant(lang_code)
+    except LookupError:
+        return current
 
 
 def send(
@@ -13,14 +25,19 @@ def send(
     content: StrOrPromise,
     recipients: list[str],
     context: dict[str, Any] | None = None,
+    lang_code: str | None = None,
 ) -> int:
-    # todo add i18n support
+    language = determine_language(lang_code)
+
     context = context or {}
     context.setdefault("title", title)
     context.setdefault("content", content)
 
     template = "mailing/%s.html" % template
-    body = render_to_string(template, context=context)
+
+    with translation.override(language):
+        body = render_to_string(template, context=context)
+
     email = EmailMessage(title, body, to=recipients)
     email.content_subtype = "html"
     return email.send()

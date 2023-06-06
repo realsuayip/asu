038b14cdb306b9353fa975f6cf3a40dd51020e3e
şuayip üzülmez <suayip.541@gmail.com>
2023-06-07 00:59:35 +0300

    Fixed #22 -- Add generic cache mechanism

--- asu/settings/test.py
+++ asu/settings/test.py
@@ -3,4 +3,9 @@ from asu.settings import *  # noqa: F403
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.MD5PasswordHasher",
 ]
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+    },
+}
 INSTALLED_APPS.remove("debug_toolbar")  # noqa: F405

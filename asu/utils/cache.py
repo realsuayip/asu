2e8b9f9150f179476aa358a3d209b5cf142d2575
şuayip üzülmez <suayip.541@gmail.com>
2023-06-11 15:06:28 +0300

    Add `vary` parameter to `cached_context`

--- asu/utils/cache.py
+++ asu/utils/cache.py
@@ -1,30 +1,149 @@
 import functools
+import operator
 from collections.abc import Callable
-from typing import ParamSpec, TypeVar
+from inspect import Parameter, signature
+from typing import Any, ParamSpec, TypeVar
 
 from django.core.cache import cache
+
+__all__ = ["cached_context"]
+
 
 P = ParamSpec("P")
 RT = TypeVar("RT")
 
 
+def get_argument(
+    parameter: Parameter,
+    name: str,
+    pos: int,
+    /,
+    *,
+    args: tuple[Any, ...],
+    kwargs: dict[str, Any],
+) -> Any:
+    """
+    Get the argument passed to `args` and `kwargs` using
+    parameter metadata (name, pos, kind).
+    """
+
+    if parameter.kind == Parameter.POSITIONAL_ONLY:
+        return args[pos]
+
+    if parameter.kind == Parameter.KEYWORD_ONLY:
+        return kwargs[name]
+
+    # Not sure if the argument is positional or keyword. We'll have to
+    # check both, starting with keyword arguments.
+    try:
+        return kwargs[name]
+    except KeyError:
+        try:
+            return args[pos]
+        except IndexError:
+            default = parameter.default
+            assert default is not Parameter.empty, "could not resolve %s" % name
+            return default
+
+
+def get_vary_string(
+    parameter: Parameter,
+    name: str,
+    pos: int,
+    attr: str,
+    key: str,
+    /,
+    *,
+    args: tuple[Any, ...],
+    kwargs: dict[str, Any],
+) -> str:
+    """
+    Get argument value, and compose a cache key using it. The resulting
+    key will look like this:
+
+        `key.vary(parameter_name.attr=argument)`
+    """
+
+    arg = get_argument(parameter, name, pos, args=args, kwargs=kwargs)
+    if not attr:
+        value = arg
+    else:
+        value = operator.attrgetter(attr)(arg)
+        name = name + "." + attr
+    return "%s.vary(%s=%s)" % (key, name, value)
+
+
+def fallback(key: str, *args: Any, **kwargs: Any) -> str:
+    """
+    Returns `key` directly in case `vary` parameter is not specified.
+    """
+    return key
+
+
+SUPPORTED_PARAM_TYPES = (
+    Parameter.KEYWORD_ONLY,
+    Parameter.POSITIONAL_ONLY,
+    Parameter.POSITIONAL_OR_KEYWORD,
+)
+
+
+def get_key_resolver(
+    f: Callable[..., Any],
+    /,
+    *,
+    key: str,
+    vary: str,
+) -> Callable[..., str]:
+    if not vary:
+        return functools.partial(fallback, key)
+
+    try:
+        lookup, attr = vary.split(".", maxsplit=1)
+    except ValueError:
+        lookup, attr = vary, ""
+    for pos, (name, parameter) in enumerate(signature(f).parameters.items()):
+        if name == lookup:
+            if parameter.kind not in SUPPORTED_PARAM_TYPES:
+                raise ValueError("parameter type %s is not supported." % parameter.kind)
+            return functools.partial(get_vary_string, parameter, name, pos, attr, key)
+    raise ValueError("no parameter named `%s` was found." % lookup)
+
+
 def cached_context(
-    *, key: str, timeout: float | None = None
+    *,
+    key: str,
+    timeout: float | None = None,
+    vary: str = "",
 ) -> Callable[[Callable[P, RT]], Callable[P, RT]]:
     """
     Cache the return value of a function using the primary
     cache backend (e.g., Redis).
+
+    :param key: Cache key to use.
+    :param timeout: Timeout for cache, set `None` to cache indefinitely.
+    :param vary: Specify a parameter name to vary the *cache key*
+    depending on the argument. You may also specify the attributes
+    of the argument. Examples: `param`, `param.attr`, `param.attr.value`.
+    The argument should have proper string representation since it is
+    going to be used in the cache key.
+
+    If `vary` is specified, the resulting cache key will look like this:
+        `key.vary(parameter_name.attr=argument)`
     """
 
     def decorator(f: Callable[P, RT]) -> Callable[P, RT]:
+        get_cache_key = get_key_resolver(f, key=key, vary=vary)
+
         @functools.wraps(f)
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> RT:
-            from_cache: RT = cache.get(key)
+            cache_key = get_cache_key(args=args, kwargs=kwargs)
+
+            from_cache: RT = cache.get(cache_key)
             if from_cache is not None:
                 return from_cache
 
             retval = f(*args, **kwargs)
-            cache.set(key, retval, timeout=timeout)
+            cache.set(cache_key, retval, timeout=timeout)
             return retval
 
         return wrapper
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,4 @@
+import types
 from unittest.mock import Mock
 
 from django.conf import settings
@@ -47,7 +48,7 @@ class TestFileUtils(TestCase):
 
 
 class TestCacheUtils(TestCase):
-    def test_cached_context(self):
+    def test_cached_context_simple(self):
         mock = Mock()
 
         @cached_context(key="asu.test.my_func")
@@ -61,3 +62,129 @@ class TestCacheUtils(TestCase):
         mock.compute.assert_called_once()
         self.assertEqual(256, cache.get("asu.test.my_func"))
         self.assertEqual("Some function.", my_func.__doc__)
+
+    def test_cached_context_pos_arg(self):
+        mock = Mock()
+
+        @cached_context(key="asu.test.fun", vary="param")
+        def fun(param, /) -> int:
+            if param == 2:
+                mock.compute()
+            return 256 + param
+
+        self.assertEqual(256 + 2, fun(2))
+        self.assertEqual(256 + 2, fun(2))
+        mock.compute.assert_called_once()
+        self.assertEqual(256 + 3, fun(3))
+
+        self.assertEqual(256 + 2, cache.get("asu.test.fun.vary(param=2)"))
+        self.assertEqual(256 + 3, cache.get("asu.test.fun.vary(param=3)"))
+
+    def test_cached_context_keyword_arg(self):
+        mock = Mock()
+
+        @cached_context(key="asu.test.fun1", vary="param")
+        def fun1(*, param) -> int:
+            if param == 2:
+                mock.compute()
+            return 257 + param
+
+        self.assertEqual(257 + 2, fun1(param=2))
+        self.assertEqual(257 + 2, fun1(param=2))
+        mock.compute.assert_called_once()
+        self.assertEqual(257 + 3, fun1(param=3))
+
+        self.assertEqual(257 + 2, cache.get("asu.test.fun1.vary(param=2)"))
+        self.assertEqual(257 + 3, cache.get("asu.test.fun1.vary(param=3)"))
+
+    def test_cached_context_keyword_or_pos_arg(self):
+        mock = Mock()
+
+        @cached_context(key="asu.test.fun2", vary="param")
+        def fun2(param) -> int:
+            if param == 2:
+                mock.compute()
+            return 257 + param
+
+        self.assertEqual(257 + 2, fun2(2))
+        self.assertEqual(257 + 2, fun2(param=2))
+        mock.compute.assert_called_once()
+        self.assertEqual(257 + 3, fun2(param=3))
+
+        self.assertEqual(257 + 2, cache.get("asu.test.fun2.vary(param=2)"))
+        self.assertEqual(257 + 3, cache.get("asu.test.fun2.vary(param=3)"))
+
+    def test_cached_context_unknown_arg(self):
+        try:
+
+            @cached_context(key="asu.test.fun3", vary="unknown")
+            def fun3(param) -> int:
+                return 257 + param
+
+        except ValueError as exc:
+            self.assertEqual("no parameter named `unknown` was found.", str(exc))
+        else:
+            self.fail("missing exception for unknown parameter")
+
+    def test_cached_context_with_attr(self):
+        mock = Mock()
+
+        @cached_context(key="asu.test.fun4", vary="param.attr")
+        def fun4(param) -> int:
+            if param.attr == 2:
+                mock.compute()
+            return 257 + param.attr
+
+        arg2 = types.SimpleNamespace(attr=2)
+        arg3 = types.SimpleNamespace(attr=3)
+
+        self.assertEqual(257 + 2, fun4(arg2))
+        self.assertEqual(257 + 2, fun4(param=arg2))
+        mock.compute.assert_called_once()
+        self.assertEqual(257 + 3, fun4(param=arg3))
+
+        self.assertEqual(257 + 2, cache.get("asu.test.fun4.vary(param.attr=2)"))
+        self.assertEqual(257 + 3, cache.get("asu.test.fun4.vary(param.attr=3)"))
+
+    def test_cached_context_unsupported_var_keyword(self):
+        try:
+
+            @cached_context(key="asu.test.fun5", vary="kwargs")
+            def fun5(*args, **kwargs) -> int:
+                return 258
+
+        except ValueError as exc:
+            self.assertEqual("parameter type VAR_KEYWORD is not supported.", str(exc))
+        else:
+            self.fail("missing exception for unsupported parameter")
+
+    def test_cached_context_unsupported_var_positional(self):
+        try:
+
+            @cached_context(key="asu.test.fun6", vary="args")
+            def fun6(*args, **kwargs) -> int:
+                return 258
+
+        except ValueError as exc:
+            self.assertEqual(
+                "parameter type VAR_POSITIONAL is not supported.", str(exc)
+            )
+        else:
+            self.fail("missing exception for unsupported parameter")
+
+    def test_cached_context_captures_default_argument(self):
+        mock = Mock()
+
+        @cached_context(key="asu.test.fun7", vary="param")
+        def fun7(param=2) -> int:
+            if param == 2:
+                mock.compute()
+            return 257 + param
+
+        self.assertEqual(257 + 2, fun7())
+        self.assertEqual(257 + 2, fun7())
+        mock.compute.assert_called_once()
+        self.assertEqual(257 + 3, fun7(3))
+
+        self.assertEqual(257 + 2, cache.get("asu.test.fun7.vary(param=2)"))
+        self.assertEqual(257 + 3, cache.get("asu.test.fun7.vary(param=3)"))

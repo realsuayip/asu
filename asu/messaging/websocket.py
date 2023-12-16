3590ebaa6a06ade46944914a1172a4eabcfa0ba4
şuayip üzülmez <suayip.541@gmail.com>
2023-12-16 14:27:30 +0300

    Refactor WebSocket authentication method

--- asu/messaging/websocket.py
+++ asu/messaging/websocket.py
@@ -1,22 +1,20 @@
-from typing import Any
-
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
-
-from asu.auth.models import User
 
 
 class ConversationConsumer(AsyncJsonWebsocketConsumer):
-    async def receive_json(self, content: dict[str, Any], **kwargs: Any) -> None:
-        ticket = content["ticket"]
-        user_id, _ = User.objects.verify_ticket(ticket, ident="websocket", max_age=10)
+    def get_group(self) -> str:
+        return "conversations_%s" % self.scope["user_id"]
 
-        if getattr(self, "group", None) is None:
-            self.group = "conversations_%s" % user_id
-            await self.channel_layer.group_add(self.group, self.channel_name)
+    async def connect(self) -> None:
+        await self.channel_layer.group_add(self.get_group(), self.channel_name)
+        await self.accept()
 
     async def disconnect(self, code: str) -> None:
-        if group := getattr(self, "group", None):
-            await self.channel_layer.group_discard(group, self.channel_name)
+        await self.channel_layer.group_discard(self.get_group(), self.channel_name)
 
     async def conversation_message(self, event: str) -> None:
         await self.send_json(event)
+
+    async def websocket_receive(self, message: str) -> None:
+        # Deny incoming frames.
+        await self.close()

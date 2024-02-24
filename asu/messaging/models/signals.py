ca81a00ba8da1c580b4f93c67e01fb862d91ca4d
şuayip üzülmez <suayip.541@gmail.com>
2024-02-24 13:47:03 +0300

    Optimize message delivery queries a little bit

--- asu/messaging/models/signals.py
+++ asu/messaging/models/signals.py
@@ -4,8 +4,12 @@ from typing import Any
 from django.db import transaction
 from django.db.models.signals import m2m_changed, post_save, pre_delete
 from django.dispatch import receiver
+from django.utils import timezone
 
 from asu.messaging.models import Conversation, ConversationRequest, Message
+
+Assignment = Conversation.messages.through
+"""m2m model that assigns messages to conversations."""
 
 
 @receiver(post_save, sender=Message, dispatch_uid="deliver_message")
@@ -15,8 +19,14 @@ def deliver_message(instance: Message, created: bool, **kwargs: Any) -> None:
 
     sender, recipient = instance.sender, instance.recipient
 
+    # If conversation objects between these two people is not created,
+    # create them. Otherwise, fetch their ID to perform related assignments.
     holder, _ = sender.conversations.get_or_create(target=recipient)
     target, _ = recipient.conversations.get_or_create(target=sender)
+
+    # Similarly, create a conversation request if not exists. This way, The
+    # user has ability to see which conversations are newly requested. Requests
+    # are automatically accepted in case recipient follows the sender.
     request, _ = ConversationRequest.objects.compose(sender, recipient)
 
     if (not request.is_accepted) and instance.has_receipt:
@@ -25,10 +35,21 @@ def deliver_message(instance: Message, created: bool, **kwargs: Any) -> None:
         instance.has_receipt = False
         instance.save(update_fields=["has_receipt"])
 
-    holder.messages.add(instance)
-    target.messages.add(instance)
-    holder.save(update_fields=["date_modified"])
-    target.save(update_fields=["date_modified"])
+    # Assign messages to conversations. Each conversation tracks the list of
+    # messages separately, this way one user can delete a message (or the
+    # whole conversation) while the other preserves it.
+    Assignment.objects.bulk_create(
+        [
+            Assignment(conversation=holder, message=instance),
+            Assignment(conversation=target, message=instance),
+        ]
+    )
+
+    # Update modification timestamps for related conversations so that when
+    # ordering by modified, conversations with recent activity rise to top.
+    Conversation.objects.filter(pk__in=[holder.pk, target.pk]).update(
+        date_modified=timezone.now()
+    )
 
     relay = partial(instance.websocket_send, target.pk)
     transaction.on_commit(relay)

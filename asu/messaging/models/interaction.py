34e2233c99bd93e317bb386a6d55090e3ef81d0e
şuayip üzülmez <suayip.541@gmail.com>
2024-10-04 00:05:57 +0300

    progress

--- asu/messaging/models/interaction.py
+++ asu/messaging/models/interaction.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from django.conf import settings
 from django.db import models
+from django.db.models import Q
 from django.utils.translation import gettext_lazy as _
 
 
@@ -18,10 +19,10 @@ class Interaction(models.Model):
         verbose_name=_("user"),
         related_name="interactions",
     )
-    event = models.ForeignKey(
-        "messaging.Event",
+    message = models.ForeignKey(
+        "messaging.Message",
         on_delete=models.CASCADE,
-        verbose_name=_("event"),
+        verbose_name=_("message"),
         related_name="interactions",
     )
     type = models.CharField(
@@ -36,6 +37,13 @@ class Interaction(models.Model):
         # todo some kind of idx(user,event,type)
         verbose_name = _("interaction")
         verbose_name_plural = _("interactions")
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "message"],
+                condition=Q(type="read"),
+                name="unique_read_interaction",
+            ),
+        ]
 
     def __str__(self) -> str:
         return str(self.pk)

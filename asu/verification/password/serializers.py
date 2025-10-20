0896b8b00447da4632c8a1c8defdf711b0b9f351
şuayip üzülmez <suayip.541@gmail.com>
2025-10-20 22:29:29 +0300

    Send password reset email in async manner

--- asu/verification/password/serializers.py
+++ asu/verification/password/serializers.py
@@ -1,39 +1,22 @@
 from typing import Any
 
-from django.db import transaction
-
 from rest_framework import serializers
 
 from asu.auth.models import User
 from asu.verification.models import PasswordResetVerification
 from asu.verification.serializers import BaseCheckSerializer
+from asu.verification.tasks import send_password_reset_email
 
 
-class PasswordResetVerificationSerializer(
-    serializers.ModelSerializer[PasswordResetVerification]
-):
-    class Meta:
-        model = PasswordResetVerification
-        fields = ("email",)
+class PasswordResetVerificationSerializer(serializers.Serializer[dict[str, Any]]):
+    email = serializers.EmailField()
 
     def validate_email(self, email: str) -> str:
         return User.objects.normalize_email(email)
 
-    @transaction.atomic
-    def create(self, validated_data: dict[str, Any]) -> PasswordResetVerification:
-        try:
-            user = User.objects.get(email=validated_data["email"])
-            if not user.has_usable_password():
-                # Consistent with Django behavior, users with unusable
-                # passwords may not reset their passwords.
-                raise ValueError
-        except (User.DoesNotExist, ValueError):
-            return validated_data  # type: ignore[return-value]
-
-        validated_data["user"] = user
-        verification = super().create(validated_data)
-        transaction.on_commit(verification.send_email)
-        return verification
+    def create(self, validated_data: dict[str, Any]) -> dict[str, Any]:
+        send_password_reset_email.delay(email=validated_data["email"])
+        return validated_data
 
 
 class PasswordResetVerificationCheckSerializer(BaseCheckSerializer):
--- asu/verification/tasks.py
+++ asu/verification/tasks.py
@@ -4,7 +4,11 @@ from celery.utils.log import get_task_logger
 
 from asu.auth.models import User
 from asu.core.celery import app
-from asu.verification.models import EmailVerification, RegistrationVerification
+from asu.verification.models import (
+    EmailVerification,
+    PasswordResetVerification,
+    RegistrationVerification,
+)
 
 logger = get_task_logger(__name__)
 
@@ -36,4 +40,23 @@ def send_email_change_email(*, user_id: int, email: str) -> None:
     )
     with transaction.atomic():
         verification = EmailVerification.objects.create(email=email, user=user)
+        transaction.on_commit(verification.send_email)
+
+
+@app.task
+def send_password_reset_email(*, email: str) -> None:
+    try:
+        user = User.objects.only("id", "password").get(email__iexact=email)
+    except User.DoesNotExist:
+        return
+    if not user.has_usable_password():
+        logger.warning(
+            "Password reset request is cancelled because user did not have"
+            " usable password, user_id=%s",
+            user.pk,
+        )
+        return
+    logger.info("Password reset requested, user_id=%s", user.pk)
+    with transaction.atomic():
+        verification = PasswordResetVerification.objects.create(user=user, email=email)
         transaction.on_commit(verification.send_email)

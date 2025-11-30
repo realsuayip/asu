558f642f833534b1ade87eae2686b8f467681fcd
şuayip üzülmez <suayip.541@gmail.com>
2025-11-30 18:53:27 +0300

    s

--- asu/verification/password/schemas.py
+++ asu/verification/password/schemas.py
@@ -1,23 +1,20 @@
+from django.utils.translation import gettext_lazy as _
+
 from drf_spectacular.utils import OpenApiExample, extend_schema
 
-from asu.core.utils.openapi import Tag, get_error_repr
+from asu.core.utils.openapi import Tag, examples, get_error_repr
 from asu.core.utils.rest import APIError
 from asu.verification.password.serializers import (
     PasswordResetSerializer,
-    PasswordResetVerificationCheckSerializer,
     PasswordResetVerificationSendSerializer,
 )
-from asu.verification.registration.schemas import consent_examples
 
 __all__ = ["password_reset"]
 
 
 send = extend_schema(
-    summary="Send password reset verification",
+    summary=_("Send password reset verification"),
     tags=[Tag.USER_PASSWORD_RESET],
-    description="The provided email will receive a code to verify the"
-    " user. The consent for password resetting process is"
-    " obtained with this code and email pair.",
     examples=[
         OpenApiExample(
             "e-mail is invalid",
@@ -33,32 +30,38 @@ send = extend_schema(
 )
 
 verify = extend_schema(
-    summary="Check password reset verification",
+    summary=_("Verify password reset verification"),
     tags=[Tag.USER_PASSWORD_RESET],
-    description="Given an e-mail (one that received verification"
-    " e-mail via related endpoint) and code, check if the pairs make"
-    " a valid combination. The returned consent is required to actually"
-    " reset the password.",
-    examples=consent_examples,
+    examples=[
+        examples.not_found,
+        OpenApiExample(
+            "fields have errors",
+            value=get_error_repr({"code": ["Please enter a valid code."]}),
+            response_only=True,
+            status_codes=["400"],
+        ),
+    ],
     responses={
-        200: PasswordResetVerificationCheckSerializer,
-        404: APIError,
+        204: None,
         400: APIError,
+        404: APIError,
     },
 )
+
 complete = extend_schema(
     summary="Reset password",
     tags=[Tag.USER_PASSWORD_RESET],
     responses={
         200: PasswordResetSerializer,
         400: APIError,
+        404: APIError,
     },
     examples=[
+        examples.not_found,
         OpenApiExample(
             "bad values",
             value=get_error_repr(
                 {
-                    "email": ["This e-mail could not be verified."],
                     "password": [
                         "This password is too common.",
                         "This password is too short. It must contain at"
@@ -68,7 +71,7 @@ complete = extend_schema(
             ),
             response_only=True,
             status_codes=["400"],
-        )
+        ),
     ],
 )
 
--- asu/verification/registration/schemas.py
+++ asu/verification/registration/schemas.py
@@ -1,9 +1,10 @@
+from django.utils.translation import gettext_lazy as _
+
 from drf_spectacular.utils import OpenApiExample, extend_schema
 
 from asu.core.utils.openapi import Tag, examples, get_error_repr
 from asu.core.utils.rest import APIError
 from asu.verification.registration.serializers import (
-    RegistrationVerificationCheckSerializer,
     RegistrationVerificationSendSerializer,
     UserCreateSerializer,
 )
@@ -12,11 +13,8 @@ __all__ = ["registration"]
 
 
 send = extend_schema(
-    summary="Send registration verification",
+    summary=_("Send registration verification"),
     tags=[Tag.USER_REGISTRATION],
-    description="Given that provided email that is not already taken,"
-    " sends an e-mail containing a six-digit that could"
-    " be used to verify the e-mail.",
     examples=[
         OpenApiExample(
             "e-mail is invalid",
@@ -31,62 +29,34 @@ send = extend_schema(
     },
 )
 
-consent_examples = [
-    OpenApiExample(
-        "success",
-        value={
-            "email": "test@example.com",
-            "code": "595767",
-            "consent": "21:1nG4Qe:VnTCqKDQvtLxLHLSt1lZ5nwGQF6gL8BQ6N_ywla_k9g",
-        },
-        response_only=True,
-        status_codes=["200"],
-    ),
-    examples.not_found,
-    OpenApiExample(
-        "fields have errors",
-        value=get_error_repr(
-            {
-                "email": ["Enter a valid email address."],
-                "code": [
-                    "Ensure this field has at least 6 digits.",
-                    "Ensure this field contains only digits.",
-                ],
-            }
-        ),
-        response_only=True,
-        status_codes=["400"],
-    ),
-]
-
-
 verify = extend_schema(
-    summary="Check registration verification",
+    summary=_("Verify registration verification"),
     tags=[Tag.USER_REGISTRATION],
-    description="Given an e-mail (one that received verification"
-    " e-mail via related endpoint) and code, check if the pairs make"
-    " a valid combination.",
-    examples=consent_examples,
+    examples=[
+        examples.not_found,
+        OpenApiExample(
+            "fields have errors",
+            value=get_error_repr({"code": ["Please enter a valid code."]}),
+            response_only=True,
+            status_codes=["400"],
+        ),
+    ],
     responses={
-        200: RegistrationVerificationCheckSerializer,
-        404: APIError,
+        204: None,
         400: APIError,
+        404: APIError,
     },
 )
 
 complete = extend_schema(
-    summary="Register a new user",
+    summary=_("Register a new user"),
     tags=[Tag.USER_REGISTRATION],
-    description="Before you send a request to this endpoint,"
-    " you need to acquire 'consent' string. This string is obtained via "
-    "email validation through registration verification flow. Check"
-    " registration verification documentation to learn more.",
     examples=[
+        examples.not_found,
         OpenApiExample(
             "bad user information",
             value=get_error_repr(
                 {
-                    "email": ["Enter a valid email address."],
                     "display_name": ["This field may not be blank."],
                     "username": [
                         "Usernames can only contain latin letters,"
@@ -98,21 +68,12 @@ complete = extend_schema(
             response_only=True,
             status_codes=["400"],
         ),
-        OpenApiExample(
-            "bad consent",
-            value=get_error_repr(
-                {
-                    "email": [
-                        "This e-mail could not be verified. Please provide a"
-                        " validated e-mail address."
-                    ]
-                }
-            ),
-            response_only=True,
-            status_codes=["400"],
-        ),
     ],
-    responses={201: UserCreateSerializer, 400: APIError},
+    responses={
+        201: UserCreateSerializer,
+        400: APIError,
+        404: APIError,
+    },
 )
 
 

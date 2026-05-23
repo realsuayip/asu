865084104541f163569e7348cee57491afcde0cc
şuayip üzülmez <suayip.541@gmail.com>
2026-05-23 21:29:50 +0300

    Adapt new Django 6.0 `MAILERS` setting

--- asu/core/settings/__init__.py
+++ asu/core/settings/__init__.py
@@ -137,12 +137,18 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND = env.str("EMAIL_BACKEND")
-EMAIL_HOST = env.str("EMAIL_HOST")
-EMAIL_PORT = env.int("EMAIL_PORT")
-EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS")
+MAILERS = {
+    "default": {
+        "BACKEND": "django.core.mail.backends.smtp.EmailBackend",
+        "OPTIONS": {
+            "host": env.str("EMAIL_HOST"),
+            "port": env.int("EMAIL_PORT"),
+            "username": env.str("EMAIL_HOST_USER"),
+            "password": env.str("EMAIL_HOST_PASSWORD"),
+            "use_tls": env.bool("EMAIL_USE_TLS"),
+        },
+    }
+}
 DEFAULT_FROM_EMAIL = env.str("DEFAULT_FROM_EMAIL")
 
 LOGIN_URL = "two_factor:login"
